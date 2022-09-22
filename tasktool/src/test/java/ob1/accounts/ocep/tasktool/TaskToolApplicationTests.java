9e688786f392e49dc2060b1c38a636ab8ec6078f
Thato-Moloi <Thato.moloi@dariel.co.za>
2022-09-22 11:57:45 +0200

    First commit.

---
+++ tasktool/src/test/java/ob1/accounts/ocep/tasktool/TaskToolApplicationTests.java
@@ -0,0 +1,13 @@
+package ob1.accounts.ocep.tasktool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
